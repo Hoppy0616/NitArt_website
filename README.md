4c263596ed2615cb4f6c7b5a14c96fd6dd162f9e
Hoppy0616 <hodahoda.20430@gmail.com>
2020-11-18 11:42:03 +0900

    changed icon name

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NitArt_website
-A group of Numazu NIT give opportunities of creation for Numazu NIT students. 
+-  We are group of Numazu NIT give opportunities of creation for Numazu NIT students. 
+- 我々は沼津高専の学生に創作する機会を作る集団、NitArtです。
---
+++ kosensai_app/src/components/Pictflort.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div class="Pic">
+        <img src="../assets/art.jpg">
+  </div>
+    
+  </div>  
+</template>
+<script>
+export default {
+  name:Pictflort,
+  props:{
+
+  }
+}
+</script>
+<style>
+
+</style>
